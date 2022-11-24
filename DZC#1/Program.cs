d87608db66b667c7811c8a3c88c22341f2ae64c9
«AlexDrukk» <drukker81@mail.ru>
2022-11-24 21:09:56 +0300

    2 задача готова

--- DZC#1/Program.cs
+++ DZC#1/Program.cs
@@ -2,12 +2,16 @@
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
+
 if (num1 > num2)
 {
     Console.WriteLine($"Максимальное число равно {num1}");
 }
-else
+if (num1 < num2)
 {
     Console.WriteLine($"Максимальное число равно {num2}");
 }
-
+else
+{
+    Console.WriteLine("Вы ввели одинаковые числа");
+}
