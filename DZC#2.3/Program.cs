52d6242d93fd576d178b49dd6d2dbf89fca9227b
«AlexDrukk» <drukker81@mail.ru>
2022-11-30 18:22:42 +0300

    15 задача готова

---
+++ DZC#2.3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 1 && num <= 5)
+Console.WriteLine("нет");
+else
+Console.WriteLine("да");
