fd09305b6ebfbade11544b49fb52f2e9eeac6441
«AlexDrukk» <drukker81@mail.ru>
2022-12-01 22:01:00 +0300

    23 задача готова

--- DZC#3.2/Program.cs
+++ DZC#3.2/Program.cs
@@ -13,5 +13,5 @@ Console.WriteLine("Введите координату Y для точки B");
 int y2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату Z для точки B");
 int z2 = Convert.ToInt32(Console.ReadLine());
-double lenght = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
-Console.WriteLine($"Расстояние между точками в 3D пространстве равно {lenght}"); 
+double lenght = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+Console.WriteLine($"Расстояние между точками в 3D пространстве равно {lenght}");
