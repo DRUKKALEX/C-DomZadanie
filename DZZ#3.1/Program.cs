a6ea32cfaf490bd81e81dc82dc2829eb6a2e491e
«AlexDrukk» <drukker81@mail.ru>
2022-12-01 20:52:13 +0300

    19 задача готова

---
+++ DZZ#3.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int first = num / 10000;
+int second = num % 10000 / 1000;
+int fourth = num % 100 / 10;
+int fifth = num % 10;
+if (first == fifth && second == fourth)
+    Console.WriteLine($"Число {num} является палиндромом");
+else
+    Console.WriteLine($"Число {num} не является палиндромом");
