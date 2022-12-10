f9e7416e922b56202504753c63e79088aeb171c3
«AlexDrukk» <drukker81@mail.ru>
2022-12-10 20:40:16 +0300

    38 задача готова

---
+++ DZC#5.34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество
+//чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+int[] rendr(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+        Console.Write($"{array[i]} "); 
+    }
+    return array;
+}
+int len = 4;
+int[] array = rendr(len);
+int FindCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        if ((array[i] % 2) == 0)
+        {
+            count = count + 1;            
+        }
+    }
+    return count;
+}
+Console.WriteLine("");
+Console.WriteLine($"{FindCount(array)}");
+
+
+
---
+++ Seminar6.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 42: Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+string result = string.Empty; 
+while (num>0)
+{
+    result = num%2 + result;
+    num = num/2;
+}
+Console.WriteLine($"Число в двоичной системе равно {result}");
